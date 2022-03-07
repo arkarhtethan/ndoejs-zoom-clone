7ab550e66b8a5bc8d9830a46b5bad33a51b19ccc
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-07 09:36:12 +0630

    server socket logic added

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = new ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
+app.use("/peerjs", peerServer);
+
 app.get("/", (req, res) => {
-  res.render("room");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.status(200).render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+  });
+  socket.on("message", (message, roomId) => {
+    io.to(roomId).emit("createMessage", message);
+  });
 });
 
 server.listen(3030);
+peerServer.on("connection", (client) => {
+  console.log("client connected");
+});
+peerServer.on("disconnect", (client) => {
+  console.log("client disconnected");
+});
